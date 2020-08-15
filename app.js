5608475b3481c564faf825f28089ba1ca44445b5
Tiago Grossinho <tiago.grossinho@gmail.com>
2020-08-15 11:45:51 +0200

    Started to implement endgame + hidden chars.

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 'use strict';
 console.log('Here\'s a hidden message');
 
+var g_inputName = '';
 var g_nbLettersOk = 0;
 
-function dragStart (ev) {
+function dragStart(ev) {
     var letterData = ev.target.getAttribute('data-letter');
     var style = window.getComputedStyle(ev.target, null);
     ev.dataTransfer.setData("id", ev.target.id);
@@ -11,11 +12,11 @@ function dragStart (ev) {
     ev.dataTransfer.setData("text/plain", (parseInt(style.getPropertyValue("left"), 10) - ev.clientX) + ',' + (parseInt(style.getPropertyValue("top"), 10) - ev.clientY));
 }
 
-function dragOver (ev) {
+function dragOver(ev) {
     ev.preventDefault();
 }
 
-function drop (ev) {
+function drop(ev) {
     ev.preventDefault();
     var thisId = ev.target.id;
     var thisLetter = ev.target.getAttribute('data-letter');
@@ -24,21 +25,28 @@ function drop (ev) {
     var offset = ev.dataTransfer.getData("text/plain").split(',');
 
     // If it is the same letter
-    if (draggedLetter == thisLetter) {
+    if (draggedLetter == thisLetter &&
+        !document.getElementById(thisId).classList.contains('okLetter')) {
 
         // Mask the source letter
         document.getElementById(draggedId).style.display = 'none';
 
         // Flag the destination letter as OK
+        document.getElementById(thisId).classList.remove('hiddenLetter');
         document.getElementById(thisId).classList.add('okLetter');
 
         // Increment the correct letters
         g_nbLettersOk += 1;
 
         //Check if all the letters are ok
+        if (g_nbLettersOk == g_inputName.length) {
+
+            // Wait 1 second then go to endgame function
+            setTimeout(endGame, 1000);
+        }
 
     } else {
-        
+
         document.getElementById(draggedId).style.left = (ev.clientX + parseInt(offset[0], 10)) + 'px';
         document.getElementById(draggedId).style.top = (ev.clientY + parseInt(offset[1], 10)) + 'px';
     }
@@ -47,27 +55,42 @@ function drop (ev) {
 function createTable() {
 
     var form = document.getElementById("formConfig");
-    var inputName = document.getElementById("inputName").value;
-    var nameLen = inputName.length;
+    g_inputName = document.getElementById("inputName").value;
+    var nameLen = g_inputName.length;
     var hiddenNb = parseInt(document.getElementById("inputHiddenNb").value);
     var mainContainer = document.getElementById("divContent");
 
     // Hide the form
     form.style.display = 'none';
 
-    //Create the destination table
+    //Create the destination table and its only row
     var table = document.createElement('table');
     table.id = 'gameTable';
     var tbody = document.createElement('tbody');
     var tr = document.createElement('tr');
+
+    // Cycle to create each cell
     for (var i = 0; i < nameLen; i++) {
+
+        // Create a new cell
         var td = document.createElement('td');
+
+        //Define its attributes and listeners
         td.id = 'gameLetter' + i.toString();
         td.classList.add('gameLetter');
-        td.setAttribute('data-letter', inputName.charAt(i));
+        td.setAttribute('data-letter', g_inputName.charAt(i));
         td.addEventListener('dragover', dragOver);
         td.addEventListener('drop', drop);
-        td.appendChild(document.createTextNode(inputName.charAt(i)));
+
+        // If hidden, mark the letter to be invisible
+        if (nameLen - i <= hiddenNb) {
+            td.classList.add('hiddenLetter');
+        }
+
+        // Add text
+        td.appendChild(document.createTextNode(g_inputName.charAt(i)));
+
+        // Add cell to row
         tr.appendChild(td);
     }
     tbody.appendChild(tr);
@@ -83,13 +106,13 @@ function createTable() {
         table.classList.add('sourceTable');
         table.id = `sourceTable${i.toString()}`;
         table.addEventListener('dragstart', dragStart);
-        table.setAttribute('data-letter', inputName.charAt(i));
+        table.setAttribute('data-letter', g_inputName.charAt(i));
         var tbody = document.createElement('tbody');
         var tr = document.createElement('tr');
         var td = document.createElement('td');
         td.classList.add('gameLetter');
         td.classList.add('moveLetter');
-        td.appendChild(document.createTextNode(inputName.charAt(i)));
+        td.appendChild(document.createTextNode(g_inputName.charAt(i)));
         tr.appendChild(td);
         tbody.appendChild(tr);
         table.appendChild(tbody);
@@ -99,4 +122,12 @@ function createTable() {
 
     document.body.addEventListener('dragover', dragOver, false);
     document.body.addEventListener('drop', drop, false);
+}
+
+function endGame() {
+    var mainContainer = document.getElementById("divContent");
+
+    if (mainContainer) {
+        mainContainer.style.display = 'none';
+    }
 }
